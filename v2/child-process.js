355d45aecbce69affda5114d3c4d412f158da705
Sunil Bagde <bagdesunil786@gmail.com>
2022-05-10 06:52:03 +0530

    prettier

--- v2/child-process.js
+++ v2/child-process.js
@@ -1,14 +1,13 @@
-const {spawn } = require("child_process");
+const { spawn } = require("child_process");
 
+const pwd = spawn("pwd");
 
-const pwd = spawn("pwd")
+pwd.stdout.pipe(process.stdout);
 
-pwd.stdout.pipe(process.stdout)
+const { HOME } = process.env;
 
-const {HOME} = process.env;
+const cat = spawn("cat", [`${HOME}/yarn.lock`]);
+cat.stdout.pipe(process.stdout);
 
-const cat = spawn("cat", [`${HOME}/yarn.lock`])
-cat.stdout.pipe(process.stdout)
-
-const shell = spawn("ls -al ~ | wc -l", {shell:true})
-shell.stdout.pipe(process.stdout)
+const shell = spawn("ls -al ~ | wc -l", { shell: true });
+shell.stdout.pipe(process.stdout);
